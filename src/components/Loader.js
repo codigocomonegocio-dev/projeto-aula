f359882f127ae0a96d877c9810f14a5f5f7cbb00
codigocomonegocio-dev <codigocomonegocio@gmail.com>
2021-06-04 20:26:30 -0300

    Estrutura react

---
+++ src/components/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import LoaderImage from 'assets/loader.gif'
+
+const Loader = () => {
+  return <img className="loader" alt="carregando..." src={LoaderImage} />
+}
+export default Loader
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Loader from 'components/Loader'
+
+const Home = () => (
+  <>
+    <Loader></Loader>
+  </>
+)
+export default Home
