2d5b0f11bc38b72dd5080b923de819646570f8dc
caueecarvalho <cau_fcarvalho@yahoo.com>
2021-06-09 13:05:58 -0400

    Caue June 9th - 1:05pm

---
+++ src/components/Selectcard.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+import casinha from '../assets/casinha.png'
+import check from '../assets/check.png'
+
+const Selectcard = () => {
+  
+  return (
+    <>
+      <div className="Selectcard">
+       
+        <h1>Caue</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default Selectcard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 import Smile from 'components/Smile'
 
+import Selectcard from 'components/Selectcard'
+
 const Home = () => (
   <>
     <Smile valor="Outro texto qualquer" cor="#333" />
+    <Selectcard/>
   </>
 )
 export default Home
