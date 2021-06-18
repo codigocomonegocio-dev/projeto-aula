bac332c669d1d28356edc29f6d3529843b4fa155
caueecarvalho <cau_fcarvalho@yahoo.com>
2021-06-17 21:51:19 -0400

    N5 - Paulo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,13 @@ import Smile from 'components/Smile'
 
 import SelectCard from 'components/SelectCard'
 
+import N5 from 'components/N5'
+
 const Home = () => (
   <>
     <Smile valor="Outro texto qualquer" cor="#333" />
     <SelectCard text='Comprar uma casa'/>
+    <N5/>
   </>
 )
 export default Home
