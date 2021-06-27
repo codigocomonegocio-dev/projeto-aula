a227c79e30a69b60c4f434b53ba98ec875123f7a
eapsFerraz <eapstecnologia@gmail.com>
2021-06-27 18:54:22 -0300

    Revisão do Componente N7 ProgressBar

--- src/components/N7.js
+++ src/components/N7.js
@@ -2,14 +2,41 @@
 import React from 'react'
 
 // Criação do componente ProgressBar
-const N7 = () => {
+const N7 = (props) => {
   return (
     <>
       <div className="N7-progress">
-        <div className="primeira"></div>
-        <div className="segunda"></div>
-        <div className="terceira"></div>
-        <div className="quarta"></div>
+        <div
+          className="primeira"
+          style={{
+            backgroundColor: props.CorA,
+            width: props.CompA,
+          }}
+        ></div>
+        <div
+          className="segunda"
+          style={{
+            backgroundColor: props.CorB,
+            width: props.CompA,
+            display: props.FanB,
+          }}
+        ></div>
+        <div
+          className="terceira"
+          style={{
+            backgroundColor: props.CorC,
+            width: props.CompC,
+            display: props.FanC,
+          }}
+        ></div>
+        <div
+          className="quarta"
+          style={{
+            backgroundColor: props.CorD,
+            width: props.CompD,
+            display: props.FanD,
+          }}
+        ></div>
       </div>
     </>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
 import React from 'react'
 // Importando componente ProgressBar
 import N7 from 'components/N7'
-
+/* propriedades do componente:
+  CorA, CompA
+  CorB, CompB, FanB
+  CorC, CompC, FanC
+  CorD, CompD, FanD
+*/
 const Home = () => (
   <>
-    <N7 ></N7>
+    <N7
+      CorA={'red'}
+      CompA={'50%'}
+      CorB={'blue'}
+      CompB={'50%'}
+      FanB={'block'}
+    ></N7>
+    <N7
+      CorA={'#1968df'}
+      CompA={'25%'}
+      CorB={'#17d28f'}
+      CompB={'25%'}
+      FanB={'block'}
+      CorC={'#fc3467'}
+      CompC={'25%'}
+      FanC={'block'}
+      CorD={'#e9eff2'}
+      CompD={'25%'}
+      FanD={'block'}
+    ></N7>
     <N7></N7>
   </>
 )
