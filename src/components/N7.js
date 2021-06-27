3144ecb5aa9f4b17f89a62afb32b6905db9d4004
eapsFerraz <eapstecnologia@gmail.com>
2021-06-27 14:00:58 -0300

    não finalizada

--- src/components/N7.js
+++ src/components/N7.js
@@ -1,80 +1,15 @@
 // Importação da Biblioteca padrão REACT
-import React, { useState } from 'react'
+import React from 'react'
 
 // Criação do componente ProgressBar
-const N7 = (props) => {
-  const [corA, setCorA] = useState(props.corA)
-
-  const montar = () => {
-    const divA = document.getElementById('primeira')
-    const divB = document.getElementById('segunda')
-    const divC = document.getElementById('terceira')
-    const divD = document.getElementById('quarta')
-
-    if (props.total === '1') {
-      divA.style.backgroundColor = props.corA //'#1968df'
-      divA.style.width = props.tamanhoA + '%'
-      divA.style.borderRadius = '10px'
-    }
-    if (props.total === '2') {
-      divA.style.backgroundColor = props.corA
-      divA.style.width = props.tamanhoA + '%'
-      divA.style.borderRadius = '10px 0 0 10px'
-
-      divB.style.display = 'block'
-      divB.style.backgroundColor = props.corB
-      divB.style.width = props.tamanhoB + '%'
-      divB.style.borderRadius = '0 10px 10px 0'
-    }
-
-    if (props.total === '3') {
-      divA.style.backgroundColor = props.corA
-      divA.style.width = props.tamanhoA + '%'
-      divA.style.borderRadius = '10px 0 0 10px'
-
-      divB.style.display = 'block'
-      divB.style.backgroundColor = props.corB
-      divB.style.width = props.tamanhoB + '%'
-
-      divC.style.display = 'block'
-      divC.style.backgroundColor = props.corC
-      divC.style.width = props.tamanhoC + '%'
-      divC.style.borderRadius = '0 10px 10px 0'
-    }
-
-    if (props.total === '4') {
-      divA.style.backgroundColor = props.corA
-      divA.style.width = props.tamanhoA + '%'
-      divA.style.borderRadius = '10px 0 0 10px'
-
-      divB.style.display = 'block'
-      divB.style.backgroundColor = props.corB
-      divB.style.width = props.tamanhoB + '%'
-
-      divC.style.display = 'block'
-      divC.style.backgroundColor = props.corC
-      divC.style.width = props.tamanhoC + '%'
-
-      divD.style.display = 'block'
-      divD.style.backgroundColor = props.corD
-      divD.style.width = props.tamanhoD + '%'
-      divD.style.borderRadius = '0 10px 10px 0'
-    }
-  }
-
+const N7 = () => {
   return (
     <>
-      {
-        (window.onload = function () {
-          montar()
-        })
-      }
-
-      <div className="N7">
-        <div id="primeira" className="primeira"></div>
-        <div id="segunda" className="segunda"></div>
-        <div id="terceira" className="terceira"></div>
-        <div id="quarta" className="quarta"></div>
+      <div className="N7-progress">
+        <div className="primeira"></div>
+        <div className="segunda"></div>
+        <div className="terceira"></div>
+        <div className="quarta"></div>
       </div>
     </>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,8 @@ import N7 from 'components/N7'
 
 const Home = () => (
   <>
-    <N7
-      total="4"
-      corA="#1968df"
-      tamanhoA={25}
-      corB="#17d28f"
-      tamanhoB={25}
-      corC="#fc3467"
-      tamanhoC={25}
-      corD="#e9eff2"
-      tamanhoD={25}
-    ></N7>
+    <N7 ></N7>
+    <N7></N7>
   </>
 )
 export default Home
