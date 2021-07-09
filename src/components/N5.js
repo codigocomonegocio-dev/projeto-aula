5904e26ed016faa05fef189f769ba9871417eff3
Andressa <andressa_lmachado@yahoo.com>
2021-07-09 18:12:40 -0300

    CSS componente N5

---
+++ src/components/N5.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import Excluir from "../assets/Icons/Excluir"
+
+const N5 = () => {
+
+
+  return (
+    <>
+
+      <div className="linha">
+        <div className="coluna"><Excluir /><input className="item" type="text" placeholder="Item"></input></div>
+        <div className="coluna"><input className="valor" type="value" placeholder="R$ 0,00"></input></div>
+      </div>
+    </>
+  )
+}
+
+export default N5
